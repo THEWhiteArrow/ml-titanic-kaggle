34aad8c683a1e0ded3502228663ca3d285cd46ea
Damian Trafialek <damian.trafialek@gmail.com>
2024-03-26 00:03:29 +0100

    add: train and test data separation

--- README.md
+++ README.md
@@ -4,13 +4,32 @@ This is a beginner-friendly machine learning project where we use Random Forest 
 
 ## Getting Started
 
+#### Default
+
 In order to run this project, you need to have Python installed on your machine. Install the dependencies using the `requirements.txt` file with `pip install -r requirements.txt`.
 In order to run the project navigate to the main directory and run the main file using the command `python main.py`.
 
-### Dataset
+#### Advanced
+
+The project utlizes random seed for reproducibility. By default it is set to `0` and runs the model only once. If you can change this behavior by running `main.py` with the following arguments:
+
+- `--seed` - set the random seed for reproducibility
+- `--runs` - set the number of runs for the model to run
+  The seed will be the sum of the number of the run and the seed. For example, if you run the model with `--seed 0 --runs 5`, the seeds will be `0, 1, 2, 3, 4`.
+  Example: `python main.py --seed 0 --runs 5`
+
+## Analysis
+
+The project will generate a submission file in the root directory of the project. The file will contain the predictions of the model on the test dataset. The file name will have format `submission_{accuracy}_{true_negatives}_{false_positives}_{false_negatives}_{true_positives}_{seed}.csv`.
+Legend:
+
+- `accuracy` - the accuracy of the model
+- `true_negatives` - the number of true negatives
+- `false_positives` - the number of false positives
+- `false_negatives` - the number of false negatives
+- `true_positives` - the number of true positives
+- `seed` - the seed used for the model
+
+## Dataset
 
 Dataset belongs to the Kaggle Titanic competition and can be found [here](https://www.kaggle.com/c/titanic/data).
-
-### Best Model
-
-My best model achieved an accuracy of `0.77990` on the test set - roughly `78%`.
--- main.py
+++ main.py
@@ -1,9 +1,38 @@
+import argparse
 from typing import Tuple
-from numpy import ndarray
+from numpy import ndarray, ravel
 import pandas as pd
 from pandas import DataFrame
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import confusion_matrix, accuracy_score
+from sklearn.metrics import accuracy_score, confusion_matrix
+from sklearn.model_selection import train_test_split
+
+
+def read_args() -> Tuple[int, int]:
+    """
+    Function that reads the command line arguments.
+
+    Returns:
+    - Tuple[int, int]: starting_seed, num_runs
+    """
+
+    # Create argument parser
+    parser = argparse.ArgumentParser(description="Process command line arguments.")
+
+    # Add flags for starting seed and number of runs
+    parser.add_argument(
+        "--seed", type=int, default=0, help="Starting seed for random number generation"
+    )
+    parser.add_argument("--runs", type=int, default=1, help="Number of runs")
+
+    # Parse command line arguments
+    args = parser.parse_args()
+
+    # Access the values of the flags
+    starting_seed = args.seed
+    num_runs = args.runs
+
+    return starting_seed, num_runs
 
 
 def load_data() -> Tuple[DataFrame, DataFrame]:
@@ -19,7 +48,7 @@ def load_data() -> Tuple[DataFrame, DataFrame]:
     return train_data, test_data
 
 
-def preprocess_data(data: DataFrame) -> Tuple[ndarray, ndarray | None]:
+def preprocess_data(original_data: DataFrame) -> Tuple[ndarray, ndarray]:
     """
     Function that preprocesses the data for the Titanic dataset.
 
@@ -27,7 +56,7 @@ def preprocess_data(data: DataFrame) -> Tuple[ndarray, ndarray | None]:
     - data: DataFrame
 
     Returns:
-    - Tuple[ndarray, ndarray | None]: x, y
+    - Tuple[ndarray, ndarray]: x, y
     """
     features_to_use = ["Pclass", "Sex", "SibSp", "Parch", "Age"]
     dependent_variable = ["Survived"]
@@ -39,6 +68,7 @@ def preprocess_data(data: DataFrame) -> Tuple[ndarray, ndarray | None]:
     Provides the possible idea of where the cabin was located and if it was closer to the lifeboats.
     Additionally there is only one record of person in T cabin so lets simplify and remove it.
     """
+    data = original_data.copy(deep=True)
     data["Cabin"] = (
         data["Cabin"]
         .fillna("Z")
@@ -69,50 +99,89 @@ def preprocess_data(data: DataFrame) -> Tuple[ndarray, ndarray | None]:
     try:
         y = data[dependent_variable]
     except Exception:
-        y = None
+        y = DataFrame({"Survived": []})
 
     # --- CATEGORIZE ENCODING AND AVOID DUMMY VARIABLES TRAP ---
     x = pd.get_dummies(x, drop_first=True)
 
-    return x.values, y.values if y is not None else None
+    return x.values, ravel(y.values)
 
 
-def main():
+def solve(
+    usable_dataframe: DataFrame, competition_dataframe: DataFrame, random_state: int = 0
+) -> Tuple[float, ndarray, DataFrame]:
     """
-    Main function that runs the entire pipeline for the Titanic dataset.
+    A function that runs the entire pipeline for the Titanic dataset.
     """
-    # --- SETUP ---
-    train_dataframe, test_dataframe = load_data()
 
     # --- PREPROCESSING ---
-    x_train, y_train = preprocess_data(train_dataframe)
-    x_test, y_test = preprocess_data(test_dataframe)
+    x_usable, y_usable = preprocess_data(usable_dataframe)
+    x_competition, y_competition = preprocess_data(competition_dataframe)
+
+    # --- SPLIT ---
+    x_train, x_test, y_train, y_test = train_test_split(
+        x_usable,
+        y_usable,
+        test_size=0.25,
+        random_state=random_state,
+    )
+    x_train, y_train = x_usable, y_usable
 
     # --- SUPERVISE ---
     model = RandomForestClassifier(n_estimators=69, criterion="entropy", random_state=0)
     model.fit(x_train, y_train)
 
-    # --- PREDICT_TEST ---
+    # --- TEST_MODEL ---
     y_test_pred = model.predict(x_test)
-    output = pd.DataFrame(
-        {"PassengerId": test_dataframe["PassengerId"], "Survived": y_test_pred}
+    cm = confusion_matrix(y_test, y_test_pred)
+    acs = accuracy_score(y_test, y_test_pred)
+
+    # print(f"Your confusion matrix:\n{cm}")
+    # print(f"Your accuracy score (%):\n{round(acs * 100, 2)}")
+
+    # --- PREDICT_COMPETITION ---
+    y_competition_pred = model.predict(x_competition)
+    output = DataFrame(
+        {
+            "PassengerId": competition_dataframe["PassengerId"],
+            "Survived": y_competition_pred,
+        }
     )
 
-    output.to_csv("submission.csv", index=False)
-    print("Your submission was successfully saved!")
-
-    # --- DISPLAY ---
-    """
-    Training data is used since no test_data is provided to evaluate.
-    """
-    y_train_pred = model.predict(x_train)
-    cm = confusion_matrix(y_train, y_train_pred)
-    acs = accuracy_score(y_train, y_train_pred)
-    print("Your confusion matrix:")
-    print(cm)
-    print("Your accuracy score (%):")
-    print(round(acs * 100, 2))
+    return float(acs), cm, output
 
 
 if __name__ == "__main__":
-    main()
+    # --- LOAD COMMAND LINE FLAGS ---
+    starting_seed, num_runs = read_args()
+
+    # --- LOAD DATA ---
+    usable_dataframe, competition_dataframe = load_data()
+
+    # --- SOLVE ---
+
+    # TODO: Implement a correct k-fold cross validation
+    max_score: float = 0.0
+    max_solution: DataFrame = DataFrame()
+    max_cm: ndarray = ndarray([])
+    max_seed: int = 0
+
+    for seed in range(starting_seed, starting_seed + num_runs):
+        score, cm, solution = solve(
+            usable_dataframe=usable_dataframe,
+            competition_dataframe=competition_dataframe,
+            random_state=seed,
+        )
+        if score > max_score:
+            max_score = score
+            max_solution = solution
+            max_cm = cm
+            max_seed = seed
+
+    print(f"Max score: {max_score} | Max seed: {max_seed}")
+    print(f"Max confusion matrix:\n{max_cm}")
+    max_solution.to_csv(
+        f"submission_{round(max_score, 5)}_{max_cm[0][0]}-{max_cm[0][1]}-{max_cm[1][0]}-{max_cm[1][1]}_{max_seed}.csv",
+        index=False,
+    )
+    print("Your max score solution was successfully saved!")
